4082b82dfdc0f22bfa8d4e633068ac9110f0274e
mandavkarswap <smandavkar2@gmail.com>
2020-02-23 14:08:08 +0530

    Added column 'Remaining' to indicate remaining amount in Receivables  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   fs/receivables-list.php

--- fs/receivables-list.php
+++ fs/receivables-list.php
@@ -16,6 +16,10 @@ $tableArr = array(
                       'viewcol' => 'Amount',
                       'align' => 'right',
                       ),
+              'remaining_amount' => array(
+                      'viewcol' => 'Remaining',
+                      'align' => 'right',
+                      ),
               'lending_date' => array(
                       'viewcol' => ' Borrow Date',
                       'align' => 'left'
@@ -31,7 +35,7 @@ $tableArr = array(
           );
 
 $result = DB::query("
-SELECT a.id, b.name, a.amount, DATE(a.lending_date) as lending_date, DATE(a.return_date) as return_date, a.comment FROM sw_fs_receivables a
+SELECT a.id, b.name, a.amount, a.remaining_amount, DATE(a.lending_date) as lending_date, DATE(a.return_date) as return_date, a.comment FROM sw_fs_receivables a
 LEFT JOIN sw_fs_borrower_master b ON a.borrower_id = b.id
 WHERE a.is_paid=0;"
           );
